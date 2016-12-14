02b1991ce5bc81c5ee154915265dadcb985e08d3
msm <jaroslaw.jedynak@cert.pl>
2016-12-14 14:49:17 +0100

    Initial commit

---
+++ userscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        4sanity
+// @namespace   msm
+// @include     *//4programmers.net*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+$("#sidebar").css('display', 'none');
+$("#index").css('paddingRight', '0');
+$("body").css('padding-top', '0');
+$("header").css('position', 'relative');
